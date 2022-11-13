4b19cb38b03b05f482d983c3e9a1fbc7960a1d1d
Robert Coomber <robertnc@gmail.com>
2022-11-12 18:13:06 -0800

    landing page

---
+++ src/LandingPage/LandingPage.js
@@ -0,0 +1,17 @@
+import "./LandingPageStyles.css"
+import "../fonts/ABCFavoritMonoEdu-Medium.otf"
+
+export default function LandingPage()
+{
+    return(
+            <div className="background">
+                <div className="center">
+                    <p className="sourcesradio">SOURCES RADIO</p>
+                    <div className="bottomrow">
+                        <p className="srcs">SRCS©</p>
+                        <p className="goodinteruption">A GOOD INTERRUPTION.2022</p>
+                    </div>
+                </div>
+            </div>
+   )
+}
